050d2812f9f0d91727c6f3d002cd63372c612f8e
Shengtian Liao <shengtian.liao@iifuture.com>
2022-07-14 23:34:03 +1000

    feature: add test command

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'cmd:test';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Command description';
+
+  /**
+   * Execute the console command.
+   *
+   * @return int
+   */
+  public function handle()
+  {
+    // place test code here
+    return 0;
+  }
+}
