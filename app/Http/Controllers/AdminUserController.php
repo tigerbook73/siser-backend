8da93a7c13e15d973507c0344523305d2552391c
Shengtian Liao <shengtian.liao@iifuture.com>
2022-07-19 08:54:02 +1000

    Fix: create admin user with password not encrypted.

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\AdminUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserController extends SimpleController
 {
   protected string $modelClass = AdminUser::class;
+
+  public function create(Request $request)
+  {
+    $this->validateUser();
+    $inputs = $this->validateCreate($request);
+
+    $adminUser = new AdminUser($inputs);
+    $adminUser->password = Hash::make($inputs['password']);
+    DB::transaction(
+      fn () => $adminUser->save()
+    );
+    return  response()->json($this->transformSingleResource($adminUser), 201);
+  }
 }
