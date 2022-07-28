06f25d51722956c49b6f9f9705c0ffd48b83f795
Shengtian Liao <shengtian.liao@iifuture.com>
2022-07-28 16:22:15 +1000

    fix: siser-synchronizier test: rollback data.

--- tests/Service/SiserSynchronizerTest.php
+++ tests/Service/SiserSynchronizerTest.php
@@ -17,13 +17,20 @@ class SiserSynchronizerTest extends TestCase
   {
     $cognito = new Provider;
 
-
     /** @var User $user */
     $user = User::first();
+    $original_level = $user->subscription_level;
     $user->subscription_level = 9;
     $user->save();
 
     $cognitoUser = $cognito->getUserByName($user->name);
     $this->assertEquals(9, $cognitoUser->subscription_level);
+
+    // change data back
+    $user->subscription_level = $original_level;
+    $user->save();
+
+    $cognitoUser = $cognito->getUserByName($user->name);
+    $this->assertEquals($original_level, $cognitoUser->subscription_level);
   }
 }
