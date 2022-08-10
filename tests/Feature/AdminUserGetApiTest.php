b2bf53950253979f03ce5e08dd95962990fbb6d1
Chin Lim <chinlim@iifuture.org>
2022-08-10 03:09:28 +0000

    Merged in SIS-114-api-tst-admin-user-get (pull request #33)
    SIS-114-api-tst-admin-user-get
    
    * Fixed as per ST's feed back to remove query string parameters.
    
    * TST admin user Get.
    
    
    Approved-by: Shengtian Liao

--- tests/Feature/AdminUserGetApiTest.php
+++ tests/Feature/AdminUserGetApiTest.php
@@ -2,14 +2,21 @@
 
 namespace Tests\Feature;
 
-use App\Models\AdminUser;
-
 class AdminUserGetApiTest extends AdminUserTestCase
 {
   public ?string $role = 'admin';
 
-  public function testAdminUserGetOk()
+  public function testAdminUserGetSuccess()
   {
-    $this->getAssert(200, 1);
+    $this->getAssert(200, $this->object->id);
+  }
+
+  public function testAdminUserGetError()
+  {
+    $this->getAssert(404, 999999999999999999);
+
+    $this->getAssert(404, -1);
+
+    $this->getAssert(404, 0);
   }
 }
