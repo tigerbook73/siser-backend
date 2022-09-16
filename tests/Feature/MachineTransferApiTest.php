075347eab279f3966e757ff9d6da96fc2b4cbe77
Chin Lim <chinlim@iifuture.org>
2022-09-16 04:09:17 +0000

    Merged in SIS-159-test-api-test-for-transfer-machine (pull request #68)
    SIS-159-test-api-test-for-transfer-machine
    
    * Testing
    
    * Job done.
    
    
    Approved-by: Shengtian Liao

--- tests/Feature/MachineTransferApiTest.php
+++ tests/Feature/MachineTransferApiTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use App\Models\GeneralConfiguration;
 use App\Models\Machine;
+use App\Models\LdsPool;
 use Tests\Trait\CognitoProviderMockup;
 
 class MachineTransferApiTest extends MachineTestCase
@@ -13,28 +14,97 @@ class MachineTransferApiTest extends MachineTestCase
 
   public ?string $role = 'admin';
 
+  private function getUserLicenseCount(int $userId)
+  {
+    return User::find($userId)->license_count;
+  }
+
+  private function getUserLdsPool(int $userId)
+  {
+    return LdsPool::where("user_id", $userId)->first();
+  }
+
   public function testMachineTransferOk()
   {
-    // TODO: test cases need to be designed
-
     // create new user
     $user = $this->postJson('/api/v1/users', [
       "create_from" => "username",
       "username" => $this->getDefaultTestUserName(),
     ])->json();
 
+    $newUserLdsPool = LdsPool::where("user_id", $user['id'])->first();
+
     $machine = Machine::first();
+
+    // get original user license count before
+    $originalUserLicenseCountBefore = $this->getUserLicenseCount($machine->user_id);
 
     // transer machine to new user
     $response = $this->postJson("/api/v1/machines/{$machine->id}/transfer", ['new_user_id' => $user['id']]);
     $response->assertStatus(200)
       ->assertJsonStructure($this->modelSchema);
 
-    // TODO
-    // assert original user (license_count)
-    // assert new user 
-    // assert lds pool
+    // get number of machine license per unit
+    $numberOfLicensePerUnit = GeneralConfiguration::getMachineLicenseUnit();
+
+    // get original user's license count after
+    $originalUserLicenseCountAfter = $this->getUserLicenseCount($machine->user_id);
+    $originalUserLdsPool = $this->getUserLdsPool($machine->user_id);
+    $this->assertEquals($originalUserLicenseCountAfter, $originalUserLicenseCountBefore - $numberOfLicensePerUnit);
+    $this->assertEquals($originalUserLdsPool->license_count, $originalUserLicenseCountAfter);
+    $this->assertEquals($originalUserLdsPool->license_free, $originalUserLicenseCountAfter);
+
+    // get new user's license count after
+    $newUserLicenseCountAfter = $this->getUserLicenseCount($user['id']);
+    $newUserLdsPool = $this->getUserLdsPool($user['id']);
+    $this->assertEquals($newUserLicenseCountAfter, $numberOfLicensePerUnit);
+    $this->assertEquals($newUserLdsPool->license_count, $numberOfLicensePerUnit);
+    $this->assertEquals($newUserLdsPool->license_free, $numberOfLicensePerUnit);
+
+    // Assert machine's user is new user
+    $machine = Machine::first();
+    $this->assertEquals($machine->user_id, $user['id']);
   }
 
-  // TODO: more test cases to come
+  public function testMachineTransferSameUserOk()
+  {
+    $machine = Machine::first();
+
+    $numberOfLicensePerUnit = GeneralConfiguration::getMachineLicenseUnit();
+
+    // get user license count before
+    $userLicenseCountBefore = $this->getUserLicenseCount($machine->user_id);
+    $userLdsPool = $this->getUserLdsPool($machine->user_id);
+    $this->assertEquals($userLicenseCountBefore, $numberOfLicensePerUnit);
+    $this->assertEquals($userLdsPool->license_count, $userLicenseCountBefore);
+    $this->assertEquals($userLdsPool->license_free, $userLicenseCountBefore);
+
+    // transer machine to new user
+    $response = $this->postJson("/api/v1/machines/{$machine->id}/transfer", ['new_user_id' => $machine->user_id]);
+    $response->assertStatus(200)
+      ->assertJsonStructure($this->modelSchema);
+
+    // get original user's license count after
+    $userLicenseCountAfter = $this->getUserLicenseCount($machine->user_id);
+    $userLdsPool = $this->getUserLdsPool($machine->user_id);
+    $this->assertEquals($userLicenseCountAfter, $userLicenseCountBefore);
+    $this->assertEquals($userLdsPool->license_count, $userLicenseCountAfter);
+    $this->assertEquals($userLdsPool->license_free, $userLicenseCountAfter);
+  }
+
+  public function testMachineTransferNonExistMachineFail()
+  {
+    $machine = Machine::first();
+
+    $response = $this->postJson("/api/v1/machines/0/transfer", ['new_user_id' => $machine->user_id]);
+    $response->assertStatus(404);
+  }
+
+  public function testMachineTransferNonExistUserFail()
+  {
+    $machine = Machine::first();
+
+    $response = $this->postJson("/api/v1/machines/{$machine->id}/transfer", ['new_user_id' => 0]);
+    $response->assertStatus(422)->assertJsonValidationErrors(['new_user_id' => 'The selected new user id is invalid.']);
+  }
 }
