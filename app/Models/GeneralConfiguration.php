d9a38dc835123a6274e5d30f8c3da7d9e0b59a8f
Shengtian Liao <shengtian.liao@iifuture.com>
2022-10-15 14:42:25 +0800

    Fix: machine-count configure

--- app/Models/GeneralConfiguration.php
+++ app/Models/GeneralConfiguration.php
@@ -19,10 +19,24 @@ class GeneralConfiguration extends BaseGeneralConfiguration
   public static function setAll(array $inputs)
   {
     DB::transaction(function () use ($inputs) {
+      $machine_license_unit = static::getMachineLicenseUnit();
+
       foreach ($inputs as $name => $value) {
         $configure = GeneralConfiguration::where('name', $name)->first();
         $configure->value = $value;
         $configure->save();
+      }
+
+      // TODO: move this to queue tasks
+      // update license count for all users for 
+      $new_machine_license_unit = static::getMachineLicenseUnit();
+      if ($machine_license_unit != $new_machine_license_unit) {
+        /** @var User[] $users */
+        $users = User::has('machines')->withCount('machines')->get();
+        foreach ($users as $user) {
+          $user->license_count = $user->machines_count * $new_machine_license_unit;
+          $user->save();
+        }
       }
     });
   }
