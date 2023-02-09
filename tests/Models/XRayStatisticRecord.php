1f76c1108d0c445e92933a4a66b4d9baf43cef32
Shengtian Liao <shengtian.l@fcl.software>
2023-02-09 04:24:17 +0000

    Update: rest api

---
+++ tests/Models/XRayStatisticRecord.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * XRayStatisticRecord
+ */
+namespace Tests\Models;
+
+/**
+ * XRayStatisticRecord
+ */
+class XRayStatisticRecord {
+
+    /** @var \DateTime $date */
+    public $date;
+
+    /** @var \Tests\Models\XRayStatisticRecordRecord $record */
+    public $record;
+
+}
---
+++ tests/Models/XRayStatisticRecordRecord.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * XRayStatisticRecordRecord
+ */
+namespace Tests\Models;
+
+/**
+ * XRayStatisticRecordRecord
+ */
+class XRayStatisticRecordRecord {
+
+    /** @var int $user */
+    public $user = 0;
+
+    /** @var int $machine */
+    public $machine = 0;
+
+    /** @var int $licensed_user */
+    public $licensed_user = 0;
+
+    /** @var int $licensed_user_1 */
+    public $licensed_user_1 = 0;
+
+}
