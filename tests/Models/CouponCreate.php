12d44f269ddf8785c09e62e98f11f73d06faa937
Shengtian Liao <shengtian.l@fcl.software>
2023-02-14 03:44:49 +0000

    Update: rest api

--- tests/Models/CouponCreate.php
+++ tests/Models/CouponCreate.php
@@ -24,10 +24,10 @@ class CouponCreate {
     /** @var int $period months. 0 means permenant*/
     public $period = 0;
 
-    /** @var \DateTime $start_date */
+    /** @var \DateTime $start_date when start_date &lt;&#x3D; today(), coupon will be activated immediately.*/
     public $start_date;
 
-    /** @var \DateTime $end_date */
+    /** @var \DateTime $end_date when end_date &lt; today() and coupon is active, coupon will be deactivated immediately.*/
     public $end_date;
 
 }
