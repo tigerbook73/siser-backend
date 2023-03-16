eb9ae20f13d98780f2de56e7f884138359e70314
Shengtian Liao <shengtian.l@fcl.software>
2023-03-16 16:25:13 +1100

    Update: rest api

--- tests/Models/SubscriptionDR.php
+++ tests/Models/SubscriptionDR.php
@@ -12,4 +12,7 @@ class SubscriptionDR {
     /** @var string $checkout_id the dr&#39;s checkout_id to create the subscription*/
     public $checkout_id = "";
 
+    /** @var string $checkout_payment_session_id the dr&#39;s checkout payment session id for make payment*/
+    public $checkout_payment_session_id = "";
+
 }
