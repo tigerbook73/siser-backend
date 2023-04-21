e5f8e44b4f53019cda700c106e4187a11ddf0a7b
Shengtian Liao <shengtian.l@fcl.software>
2023-04-21 10:50:55 +1000

    Refactor: rebuild-data.sh

--- bin/rebuild-data.sh
+++ bin/rebuild-data.sh
@@ -6,7 +6,14 @@ set -x
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 php $BASEDIR/../artisan migrate:fresh
+
+if [ "$1" == "--model" ]; then
+  php $BASEDIR/../artisan code:models
+fi
+
 php $BASEDIR/../artisan db:seed
 php $BASEDIR/../artisan dr:cmd clear
 php $BASEDIR/../artisan dr:cmd init
+
+rm -rf $BASEDIR/../storage/logs/laravel.log
 
--- bin/rebuild-model.sh
+++ bin/rebuild-model.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-set -e
-set -x
-
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-php $BASEDIR/../artisan migrate:fresh
-php $BASEDIR/../artisan code:models
-php $BASEDIR/../artisan db:seed
-php $BASEDIR/../artisan dr:cmd clear
-php $BASEDIR/../artisan dr:cmd init
-
+$BASEDIR/rebuild-data.sh --model
