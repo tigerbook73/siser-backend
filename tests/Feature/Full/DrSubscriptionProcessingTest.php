3653f7fed2e3d20bdad8e1275d3171fb54ed46a0
Shengtian Liao <shengtian.l@fcl.software>
2023-05-16 17:55:37 +1000

    Test: more DrSubscriptionProcessingTest

--- tests/Feature/Full/DrSubscriptionProcessingTest.php
+++ tests/Feature/Full/DrSubscriptionProcessingTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature\Full;
 
 use App\Models\Subscription;
-use Carbon\Carbon;
+use DigitalRiver\ApiSdk\Model\Order as DrOrder;
+use Exception;
+use Illuminate\Support\Facades\Notification;
 use Tests\DR\DrApiTestCase;
 
 class DrSubscriptionProcessingTest extends DrApiTestCase
@@ -27,6 +29,42 @@ class DrSubscriptionProcessingTest extends DrApiTestCase
     $subscription = $this->init_processing();
 
     return $this->onOrderComplete($subscription);
+  }
+
+  public function test_processing_to_active_invoice_completing_error()
+  {
+    $subscription = $this->init_processing();
+
+    // mock up
+    $this->drMock
+      ->shouldReceive('activateSubscription')
+      ->once()
+      ->andThrow(new Exception('test', 444));
+    Notification::fake();
+
+    // call api
+    $response = $this->sendOrderComplete($this->drHelper->createOrder(
+      $subscription,
+      null,
+      DrOrder::STATE_COMPLETE
+    ), $eventId = $this->drHelper->uuid());
+
+    // refresh data
+    $subscription->refresh();
+
+    // assert
+    $response->assertStatus(400);
+    $this->assertTrue($subscription->status == 'processing');
+    $this->assertTrue($subscription->getActiveInvoice() == null);
+    $this->assertDatabaseHas('critical_sections', [
+      'type' => 'subscription',
+      'status' => 'open',
+      'object_id' => $subscription->id
+    ]);
+    $this->assertDatabaseMissing('dr_events', [
+      'event_id' => $eventId
+    ]);
+    Notification::assertNothingSent();
   }
 
   public function test_processing_to_failed_blocked()
