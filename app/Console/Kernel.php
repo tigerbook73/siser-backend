974c7943b740994cf19e5b87ea57853d2652f064
Shengtian Liao <shengtian.l@fcl.software>
2023-11-07 01:21:27 +0000

    Merged in fix/subscription-renew-annual-scheduler (pull request #206)
    fix/subscription-renew-annual-scheduler
    
    * Fix: subscription:renew-annual scheduler
    
    
    Approved-by: Shengtian Liao

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
 
     $schedule->call(fn () => Artisan::queue('subscription:clean-draft'))->everyThirtyMinutes()->name('queue subscription:clean-draft');
     $schedule->call(fn () => Artisan::queue('subscription:stop-cancelled'))->hourly()->name('queue subscription:stop-cancelled');
-    $schedule->call(fn () => Artisan::queue('subscription:subscription:renew-annual'))->hourly()->name('queue subscription:renew-annual');
+    $schedule->call(fn () => Artisan::queue('subscription:renew-annual'))->hourly()->name('queue subscription:renew-annual');
     $schedule->call(fn () => Artisan::queue('subscription:warn-pending'))->daily()->name('queue subscription:warn-pending');
   }
 
