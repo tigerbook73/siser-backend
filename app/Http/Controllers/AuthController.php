fed138190f05501fe2eb40a29d844404ae119b1d
Shengtian Liao <shengtian.l@fcl.software>
2024-05-06 10:57:19 +1000

    Fix: disable crawl index
    remove no password for user#.test account only

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -137,7 +137,11 @@ class AuthController extends Controller
 
   public function loginTest(Request $request)
   {
-    $name = $request->name ?: 'user1.test';
+    $name = $request->name ?: '';
+    if (!preg_match('/^user\d+\.test$/', $name)) {
+      return response('Invalid user name!!', 400);
+    }
+
     $user = User::where('name', $name)->first();
 
     if (!$user) {
