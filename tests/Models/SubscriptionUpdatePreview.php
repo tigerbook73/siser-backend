a22748d925536c28a6a33c12345b4f304b615da3
Shengtian Liao <shengtian.l@fcl.software>
2025-04-16 10:00:28 +1000

    Add: create SubscriptionUpdatePreview and SubscriptionUpdateRequest model classes

---
+++ tests/Models/SubscriptionUpdatePreview.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * SubscriptionUpdatePreview
+ */
+namespace Tests\Models;
+
+/**
+ * SubscriptionUpdatePreview
+ */
+class SubscriptionUpdatePreview {
+
+    /** @var \Tests\Models\Subscription $subscription */
+    public $subscription;
+
+    /** @var \Tests\Models\Subscription $subscription_preview */
+    public $subscription_preview;
+
+    /** @var array<string,mixed> $paddle_preview */
+    public $paddle_preview;
+
+}
---
+++ tests/Models/SubscriptionUpdateRequest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * SubscriptionUpdateRequest
+ */
+namespace Tests\Models;
+
+/**
+ * SubscriptionUpdateRequest
+ */
+class SubscriptionUpdateRequest {
+
+    /** @var int $license_quantity */
+    public $license_quantity = 0;
+
+}
